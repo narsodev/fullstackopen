929933f4a09e6e536eb01ffc432d90459e540f89
narsodev <narsodev@gmail.com>
2022-07-25 16:55:57 +0200

    1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <li>
+      {text}: {value}
+    </li>
+  )
+}
+
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
   
@@ -11,15 +19,24 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <ul>
-      <li>good: {good}</li>
-      <li>neutral: {neutral}</li>
-      <li>bad: {bad}</li>
-      <li>all: {all}</li>
-      <li>average: {average}</li>
-      <li>positive: {positive} %</li>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${positive} %`} />
     </ul>
   )
 }
+
+const Button = ({ text, handleClick }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -31,9 +48,18 @@ const App = () => {
       <h1>Unicafe</h1>
       <section>
         <h2>Give feedback</h2>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button
+          handleClick={() => setGood(good + 1)}
+          text="good" 
+        />
+        <Button
+          handleClick={() => setNeutral(neutral + 1)}
+          text="neutral"
+        />
+        <Button
+          handleClick={() => setBad(bad + 1)}
+          text="bad"
+        />
       </section>
       <section>
         <h2>Statistics</h2>
