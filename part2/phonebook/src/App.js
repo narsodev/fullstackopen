76eaa43b7e33f81646007a86a99793e23435edc6
narsodev <narsodev@gmail.com>
2022-08-03 20:17:20 +0200

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,22 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNewPerson = event => {
     event.preventDefault()
-    
+
     const alreadyExists = persons.some(person => person.name === newName)
     if (alreadyExists)
       return alert(`${newName} is already added to phonebook`)
 
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons([...persons, newPerson])
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -28,13 +33,16 @@ const App = () => {
           name: <input value={newName} onChange={event => setNewName(event.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={event => setNewNumber(event.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {
-          persons.map(person => <p key={person.name}>{person.name}</p>)
+          persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
         }
       </div>
     </div>
