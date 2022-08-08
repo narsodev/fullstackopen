155de4e9d346ef87b54ae8020f6f1c36b551a02a
narsodev <narsodev@gmail.com>
2022-08-08 22:29:26 +0200

    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -4,29 +4,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 const app = express()
 
 app.use(cors())
@@ -113,14 +90,19 @@ app.delete('/api/persons/:id', (req, res, next) => {
     .catch(next)
 })
 
-app.get('/info', (req, res) => {
-  const currentDate = new Date()
+app.get('/info', (req, res, next) => {
+  Person
+    .count({})
+    .then(count => {
+      const currentDate = new Date()
 
-  const infoMessage =
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${currentDate}</p>`
+      const infoMessage =
+        `<p>Phonebook has info for ${count} people</p>
+        <p>${currentDate}</p>`
 
-  res.send(infoMessage)
+      res.send(infoMessage)
+    })
+    .catch(next)
 })
 
 const errorHandler = (err, req, res, next) => {
