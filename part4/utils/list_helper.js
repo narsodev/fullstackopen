537cd17445eae6b59ac7884fa34d152a56927021
narsodev <narsodev@gmail.com>
2022-08-10 01:50:33 +0200

    4.5*: helper functions and unit tests, step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,7 +3,23 @@ const dummy = blogs => 1
 const totalLikes = blogs => blogs
   .reduce((prev, cur) => prev + cur.likes, 0)
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return null
+
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+  const mostLiked = blogs.find(blog => blog.likes === maxLikes)
+
+  const { title, author, likes } = mostLiked
+
+  return {
+    title,
+    author,
+    likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
