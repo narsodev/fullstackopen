90ebe15f6b1974ded92ea292ff4db631f236d682
narsodev <narsodev@gmail.com>
2022-08-10 00:27:13 +0200

    Check MongoDB connection

--- part4/index.js
+++ part4/index.js
@@ -3,9 +3,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 const Blog = require('./models/blog')
 
 mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Server connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
