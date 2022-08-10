aaaa9e8278002b7236cf6b725c69a64be5217aba
narsodev <narsodev@gmail.com>
2022-08-10 13:35:53 +0200

    Extract app in its own module

--- part4/index.js
+++ part4/index.js
@@ -1,29 +1,10 @@
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http')
+const app = require('./app')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('Server connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('Error connecting to MongoDB:', error.message)
-  })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
