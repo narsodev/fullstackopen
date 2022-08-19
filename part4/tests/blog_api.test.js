4bd7df80bec92f812f3247e359d062e9d64256c2
narsodev <narsodev@gmail.com>
2022-08-19 08:06:55 +0200

    4.8: Blog list tests, step1

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogs = helper.initialBlogs.map(blog => new Blog(blog))
+  await Promise.all(blogs.map(blog => blog.save()))
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ part4/tests/test_helper.js
@@ -0,0 +1,25 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'No todo es un objeto en JavaScript',
+    author: 'midudev',
+    url: 'https://midu.dev/no-todo-objeto-javascript',
+    likes: 73
+  },
+  {
+    title: 'Full Stack Open 2022',
+    author: 'University of Helsinki',
+    url: 'https://fullstackopen.com/en',
+    likes: 1156
+  }
+]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs(blog => blog.toJSON())
+}
+
+const helper = { initialBlogs, blogsInDb }
+
+module.exports = helper
