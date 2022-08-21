e3ea6bf13ad606cae7164f80d64c8b8ca4d18a53
narsodev <narsodev@gmail.com>
2022-08-21 15:38:44 +0200

    4.15: bloglist expansion, step3

--- part4/tests/user_api.test.js
+++ part4/tests/user_api.test.js
@@ -25,6 +25,30 @@ describe('when there is initially some users saved', () => {
   })
 })
 
+describe('addition of a new user', () => {
+  test('a valid user can be added', async () => {
+    const newUser = {
+      username: 'tester',
+      name: 'Tester',
+      password: 'T3ST1NG 1S GR3AT!'
+    }
+
+    const { body: createdUser } = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(createdUser).toHaveProperty('username', newUser.username)
+    expect(createdUser).toHaveProperty('name', newUser.name)
+
+    const usersAtEnd = await helper.usersInDb()
+    const usernamesOfUsersAtEnd = usersAtEnd.map(user => user.username)
+
+    expect(usernamesOfUsersAtEnd).toContain(newUser.username)
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
