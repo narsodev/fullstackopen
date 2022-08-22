0548f0863ea07b8270dc811e378248ce3bafce71
narsodev <narsodev@gmail.com>
2022-08-22 19:32:32 +0200

    4.19: bloglist expansion, step7

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -11,20 +12,36 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFromRequest = request => {
+  const authorization = request.get('authorization')
+  const isBearer = authorization && authorization.toLowerCase().startsWith('bearer ')
+
+  if (isBearer) return authorization.substring(7)
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const { title, url } = request.body
-
   if (!title && !url) return response.status(400).end()
 
-  const userOwner = await User.findOne({})
+  const token = getTokenFromRequest(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog({
-    user: userOwner.id,
+    user: user.id,
     ...request.body
   })
 
   const savedBlog = await blog.save()
-  userOwner.blogs = userOwner.blogs.concat(savedBlog.id)
-  await userOwner.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -17,6 +17,22 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message
+    })
+  }
+
+  if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+
   logger.error(error.message)
 
   next(error)
