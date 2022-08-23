641d7f13d2d9ece621bc87d4508aa3c6f2b2a937
narsodev <narsodev@gmail.com>
2022-08-23 22:50:58 +0200

    5.7* Blog list frontend, step7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
 
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const handleDetailsVisibilityChange = () => setDetailsVisible(visible => !visible)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button type='button' onClick={handleDetailsVisibilityChange}>
+        { detailsVisible ? 'hide' : 'view' }
+      </button>
+      { detailsVisible &&
+        <div>
+            <div>url: {blog.url}</div>
+            <div>likes {blog.likes} <button>like</button></div>
+            <div>user: {blog.user?.name || 'anonymous'}</div>
+        </div>
+      }
+    </div>    
+  )
+}
 export default Blog
