0351d3a7d8aff6a2b6852419d63ac50985399509
narsodev <narsodev@gmail.com>
2022-08-24 20:43:21 +0200

    Add route to reset db for testing

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config({ path: '.env.local' })
 
-const { PORT } = process.env
+const { PORT, NODE_ENV } = process.env
 
-const MONGODB_URI = process.env.NODE_ENV === 'test'
+const MONGODB_URI = NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
-module.exports = { MONGODB_URI, PORT }
+module.exports = { MONGODB_URI, PORT, NODE_ENV }
