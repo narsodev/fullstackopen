4feb1c4008d06a4e77b3741a42bcb3b149e35443
narsodev <narsodev@gmail.com>
2022-08-24 15:20:50 +0200

    5.13: Blog list tests, step1

---
+++ part5/src/components/Blog/Blog.test.js
@@ -0,0 +1,45 @@
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Blog from './index'
+
+const blog = {
+  title: 'test',
+  author: 'tester',
+  user: {
+    username: 'narso2',
+    name: 'narso2',
+    id: '6305494c613e757f6bd164b4'
+  },
+  url: 'fdff',
+  likes: 6,
+  id: '630549e5613e757f6bd164cd'
+}
+
+const user = blog.user.username
+const handleLike = () => {}
+const handleDelete = () => {}
+
+describe('<Blog />', () => {
+  beforeEach(() => {
+    render(
+      <Blog
+        blog={blog}
+        user={user}
+        handleDelete={handleDelete}
+        handleLike={handleLike}
+      />)
+  })
+
+  test(
+    'a blog renders the blog\'s title and author, but does not render its url or number of likes by default',
+    () => {
+      screen.getByText(`${blog.title} ${blog.author}`)
+
+      const url = screen.queryByText(`url: ${blog.url}`)
+      expect(url).toBeNull()
+
+      const likes = screen.queryByText(`likes ${blog.likes}`)
+      expect(likes).toBeNull()
+    }
+  )
+})
