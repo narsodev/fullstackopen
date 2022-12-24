55ad6bada0da1cf19821fa2e5406e8db4cc84f67
narsodev <narsodev@gmail.com>
2022-12-24 12:05:07 +0100

    6.13 Anecdotes and the backend, step1

--- part6/anecdotes/src/App.js
+++ part6/anecdotes/src/App.js
@@ -1,14 +1,26 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { getAll } from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    getAll().then(anecdotes => {
+      dispatch(setAnecdotes(anecdotes))
+    })
+  }, [dispatch])
+
   return (
     <>
       <h1>Anecdotes</h1>
-      <Filter />
       <Notification />
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </>
---
+++ part6/anecdotes/src/services/anecdotes.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://localhost:3001/anecdotes'
+
+const instance = axios.create({
+  baseURL: BASE_URL
+})
+
+export const getAll = async () => {
+  const { data } = await instance.get()
+  return data
+}
